be59b89564d0fb2cf57e724908b765200ad044b4
SURESH.app <hello@suresh.app>
2025-07-27 06:30:25 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,27 @@ html {
         `}</style>
       </head>
       <body>{children}</body>
+      const YEAR = new Date().getFullYear()
+
+export default {
+  footer: (
+    <small style={{ display: 'block', marginTop: '8rem' }}>
+      <time>{YEAR}</time> © Suresh Kaleyannan.
+      <a href="/feed.xml">RSS</a>
+      <style jsx>{`
+        a {
+          float: right;
+        }
+        @media screen and (max-width: 480px) {
+          article {
+            padding-top: 2rem;
+            padding-bottom: 4rem;
+          }
+        }
+      `}</style>
+    </small>
+  )
+}
     </html>
   )
 }
