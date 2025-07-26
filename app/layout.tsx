996f445efd9bf860835f3f0756bc7ffc88fbd0ea
SURESH.app <hello@suresh.app>
2025-07-27 07:15:10 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Invoice Generator',
+  description: 'Invoice Generator App',
+  generator: 'suresh.app',
 }
 
 export default function RootLayout({
